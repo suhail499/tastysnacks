9f76b9a66ba0c929625131ec64856ee23d2eb2cf
suhail499 <suhailboysuhailboy61@gmail.com>
2025-05-22 17:52:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-
-function updateCurrency() {
-  const currency = document.getElementById("currency").value;
-  const prices = document.querySelectorAll(".price");
-  prices.forEach(p => {
-    const value = p.dataset[currency.toLowerCase()];
-    p.textContent = currency === "INR" ? `₹${value}` : `$${value}`;
-  });
-}
-
 function addToCart(product, price) {
   const cart = document.getElementById("cart");
-  const li = document.createElement("li");
-  li.textContent = product + " - ₹" + price;
-  cart.appendChild(li);
+  const item = document.createElement("li");
+  item.textContent = `${product} - ₹${price}`;
+  cart.appendChild(item);
 }
